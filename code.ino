189efb1a2ae754b7ff7e3d75759848e6ab896bfa
BIPBIPGaminG <53536865+BIPBIPGaminG@users.noreply.github.com>
2022-05-15 15:50:49 +0200

    Update code.ino

--- code.ino
+++ code.ino
@@ -8,10 +8,26 @@ void setup() {
 
 void loop() {
   co3.update();
-
+  
+  co3.writeLCD("co2: " + String(co3.getCO2()), 0, 0);
+  co3.writeLCD(String(co3.getTemperature()) + "°C, 0, 10);
+  co3.writeLCD("cov: " + String(co3.getCO2()), 1, 0);
+  
   if((500 < co3.getCO2() < 800) || (250 < co3.getCOV() < 2000)){
-    Serial.println("attention");
+  
+    co3.setLCDColor(128, 0, 0);
+    delay(500);
+  
   }else if (co3.getCO2() >= 800 || co3.getCOV() >= 2000) {
-    Serial.println("alerte");
+    
+    co3.setLCDColor(255, 0, 0);
+    co3.bip(true);
+    
+    delay(500);
+    co3.bip(false);
+    
   }
+  
+  co3.setLCDColor(128, 128, 128);
+  delay(2000);
 }
